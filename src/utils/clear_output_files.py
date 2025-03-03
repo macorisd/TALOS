96628f8df8c257ecd3b2e1630156a7d4f7bc6e32
Macorís Decena <macorisd@gmail.com>
2025-03-03 14:10:21 +0100

    Refactor clear_output_files to remove files and directories in the segmentation output path

--- src/utils/clear_output_files.py
+++ src/utils/clear_output_files.py
@@ -4,11 +4,13 @@
     # src/location/output_location
 
 import os
+import shutil
 
 directories = [
     "src/tagging/lvlm_llm_tagging/lvlm_description/output_descriptions",
     "src/tagging/output_tags",
-    "src/location/output_location"
+    "src/location/output_location",
+    "src/segmentation/output_segments"
 ]
 
 for directory in directories:
@@ -16,6 +18,8 @@ for directory in directories:
         file_path = os.path.join(directory, filename)
         try:
             if os.path.isfile(file_path):
-                os.unlink(file_path)
+                os.remove(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
         except Exception as e:
             print(f"Error deleting {file_path}: {e}")
